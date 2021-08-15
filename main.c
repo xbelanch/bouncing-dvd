1211d44c134be8a0e5a207a7e1a57d150644a170
Xavier Belanche <xbelanch@protonmail.com>
2021-08-15 18:37:32 +0200

    Nice to see a dummy Bouncing DVD logo screensaver

--- main.c
+++ main.c
@@ -9,8 +9,10 @@
 
 #define WINDOWS_WIDTH 640
 #define WINDOWS_HEIGHT 480
-#define UNPACK_COLOR(color) (color >> 24), (color >> 16 & 0xff), (color >> 8 & 0xff), (color & 0xff)
-#define COLOR_PALETTE (Uint32[]){\
+#define UNPACK_RGBA_COLOR(color) (color >> 24), (color >> 16 & 0xff), (color >> 8 & 0xff), (color & 0xff)
+#define UNPACK_RGB_COLOR(color)  (color >> 24), (color >> 16 & 0xff), (color >> 8 & 0xff)
+
+#define COLOR_PALETTE (Uint32[]){               \
         0x0f380fff,              \
         0x306230ff,              \
         0x8bac0fff,              \
@@ -28,6 +30,7 @@ typedef struct {
     float xSpeed, ySpeed;
     Uint32 born, lastUpdate;
     Uint32 color;
+    SDL_Texture *texture;
 } BouncingLogo;
 
 BouncingLogo DVD;
@@ -35,17 +38,29 @@ int W0, H0;
 
 SDL_Renderer *renderer;
 SDL_Texture *texture;
+SDL_Surface* surface;
 Uint32 initial_ticks, elapsed_ms;
 size_t frames = 0;
 
 void draw_rect(SDL_Renderer *renderer, float x, float y, float w, float h, Uint32 color) {
+
     // A rectangle, with the origin at the upper left (integer)
     SDL_Rect rect = {
         (int) ceilf(x), (int) ceilf(y), // x, y
         (int) ceilf(w), (int) ceilf(h) // w, h
     };
-    SDL_SetRenderDrawColor(renderer, UNPACK_COLOR(color));
-    SDL_RenderFillRect(renderer, &rect);
+
+    // Paint a rectangle
+    // SDL_SetRenderDrawColor(renderer, UNPACK_RGBA_COLOR(color));
+    // SDL_RenderFillRect(renderer, &rect);
+
+    // Paint the DVD Logo
+    SDL_SetTextureColorMod(DVD.texture, UNPACK_RGB_COLOR(color));
+    if (SDL_RenderCopyEx(renderer, DVD.texture, NULL, &rect, 0, NULL, SDL_FLIP_NONE) != 0) {
+        fprintf(stdout, "Cannot blit to screen: %s\n", SDL_GetError());
+        exit(1);
+    }
+
 }
 
 int main(int argc, char* argv[])
@@ -54,25 +69,6 @@ int main(int argc, char* argv[])
     (void)argv[0];
 
     srand(time(NULL));
-
-    // Load logo DVD image
-    int req_format = STBI_rgb_alpha;
-    int width, height, orig_format;
-    unsigned char* data = stbi_load("./assets/sprites/DVD_logo.png", &width, &height, &orig_format, req_format);
-    if (data == NULL) {
-        SDL_Log("Loading image failed: %s", stbi_failure_reason());
-        exit(1);
-    }
-
-    SDL_Surface* surface = SDL_CreateRGBSurfaceWithFormatFrom((void*)data, width, height, 32, 4 * width, SDL_PIXELFORMAT_RGBA32);
-
-    if (surface == NULL) {
-        SDL_Log("Creating surface failed: %s", SDL_GetError());
-        stbi_image_free(data);
-        exit(1);
-    } else {
-        SDL_SaveBMP(surface, "./assets/sprites/DVD_logo.bmp");
-    }
 
     {  /* DVD Logo Initialization */
         DVD = (BouncingLogo){
@@ -100,8 +96,52 @@ int main(int argc, char* argv[])
     // The effective displacement
     W0 = WINDOWS_WIDTH - DVD.width;
     H0 = WINDOWS_HEIGHT - DVD.height;
-    fprintf(stdout, "W0 %d\n", W0);
-    fprintf(stdout, "H0 %d\n", H0);
+
+    // Load logo DVD image
+    int req_format = STBI_rgb_alpha;
+    int width, height, orig_format;
+    unsigned char* data = stbi_load("./assets/sprites/DVD_logo.png", &width, &height, &orig_format, req_format);
+
+    if (data == NULL) {
+        SDL_Log("Loading image failed: %s", stbi_failure_reason());
+        exit(1);
+    }
+
+    // Set up the pixel format color masks for RGB(A) byte arrays.
+    // Only STBI_rgb (3) and STBI_rgb_alpha (4) are supported here!
+    Uint32 rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (req_format == STBI_rgb) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else // little endian, like x86
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (req_format == STBI_rgb) ? 0 : 0xff000000;
+#endif
+
+    int depth, pitch;
+    if (req_format == STBI_rgb) {
+        depth = 24;
+        pitch = 3*width; // 3 bytes per pixel * pixels per row
+    } else { // STBI_rgb_alpha (RGBA)
+        depth = 32;
+        pitch = 4*width;
+    }
+
+    // ...and copy data to a SDL Surface
+    fprintf(stdout, "width %d height %d orig_format %d depth %d pitch %d\n", width, height, orig_format, depth, pitch);
+
+    surface = SDL_CreateRGBSurfaceFrom((void*)data, width, height, depth, pitch, rmask, gmask, bmask, amask);
+
+    if (surface == NULL) {
+        SDL_Log("Creating surface failed: %s", SDL_GetError());
+        stbi_image_free(data);
+        exit(1);
+    }
 
     { /* Init all SDL Subsystems */
         if (SDL_Init(SDL_INIT_VIDEO != 0)) {
@@ -128,13 +168,20 @@ int main(int argc, char* argv[])
         renderer =  SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
         // Set the color used for drawing operations (Rect, Line and Clear).
-        SDL_SetRenderDrawColor(renderer, UNPACK_COLOR(BACKGROUND));
+        SDL_SetRenderDrawColor(renderer, UNPACK_RGBA_COLOR(BACKGROUND));
 
         // Create a texture for a rendering context.
         texture =  SDL_CreateTexture(renderer,
                                               SDL_PIXELFORMAT_RGBA32,
                                               SDL_TEXTUREACCESS_STATIC,
                                                   WINDOWS_WIDTH, WINDOWS_HEIGHT);
+
+        // Create a texture from a surface
+        DVD.texture = SDL_CreateTextureFromSurface(renderer, surface);
+        if (DVD.texture == NULL) {
+            fprintf(stdout, "Cannot create texture from surface: %s\n", SDL_GetError());
+            exit(1);
+        }
     }
 
 
@@ -206,7 +253,7 @@ int main(int argc, char* argv[])
         { /* render() */
 
             // Clear the current rendering target with the drawing color
-            SDL_SetRenderDrawColor(renderer, UNPACK_COLOR(BACKGROUND));
+            SDL_SetRenderDrawColor(renderer, UNPACK_RGBA_COLOR(BACKGROUND));
             SDL_RenderClear(renderer);
 
             // Draw a rectangle on the current rendering target with the drawing color
